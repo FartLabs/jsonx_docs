9496ae3b80501a8125d4ba0fbffebeb32b2c205b
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-18 13:45:18 -0700

    wip

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,8 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>docs</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <title>jsonx | Documentation</title>
       </head>
       <body>
         <Component />
