54470eb757e926afd0de1fc1b1b98cf2c6e04fb1
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-18 17:32:24 -0700

    wip

--- server/examples/examples.ts
+++ server/examples/examples.ts
@@ -1,6 +1,18 @@
 /**
  * getExampleByName gets an example by example name.
  */
-export function getExampleByName(name: string): Promise<string> {
-  return Deno.readTextFile(new URL(import.meta.resolve("./" + name)));
+export async function getExampleByName(name: string): Promise<string> {
+  const text = await Deno.readTextFile(
+    new URL(import.meta.resolve("./" + name)),
+  );
+  return trimJSXImportSource(text);
+}
+
+function trimJSXImportSource(code: string): string {
+  const jsxImportSource = "/** @jsxImportSource @fartlabs/jsonx */\n\n";
+  if (code.startsWith(jsxImportSource)) {
+    return code.substring(jsxImportSource.length);
+  }
+
+  return code;
 }
