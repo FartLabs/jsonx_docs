91de5da960b2704bcbbf283ea3b8b268c5847549
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-03-20 22:00:30 -0700

    wip

--- server/docs/docs.ts
+++ server/docs/docs.ts
@@ -57,9 +57,9 @@ export function parseDocs(input: DocsInput): Docs {
 }
 
 /**
- * TableOfContentsChild represents a child of the table of contents.
+ * FSItem is an item represented in the file system.
  */
-export interface TableOfContentsChild {
+export interface FSItem {
   name: string[];
   title: string;
 
@@ -73,10 +73,7 @@ export interface TableOfContentsChild {
  * TableOfContents represents the table of contents for the documentation.
  */
 export interface TableOfContents {
-  children: (
-    & TableOfContentsChild
-    & { children?: TableOfContentsChild[] }
-  )[];
+  children: Node<FSItem>[];
 }
 
 /**
@@ -85,18 +82,14 @@ export interface TableOfContents {
 export async function getTableOfContents(
   root = ["server", "docs"],
 ): Promise<TableOfContents> {
-  const children: TableOfContentsChild[] = [];
-  const walkIt = walk(join(...root), { exts: [".md"] });
+  const children: FSItem[] = [];
+  const walkIt = walk(join(...root), { exts: [".md"], includeDirs: false });
   for await (const file of walkIt) {
-    if (file.isDirectory) {
-      continue;
-    }
-
     const path = parse(file.path);
     const name = [path.name];
     const content = await Deno.readTextFile(file.path);
     const docs = parseDocs({ name, content });
-    const child: TableOfContentsChild = { name, title: docs.title };
+    const child: FSItem = { name, title: docs.title };
     if (path.dir !== "") {
       // https://discord.com/channels/684898665143206084/684898665151594506/1217030758686785556
       const parents = path.dir.split(SEPARATOR_PATTERN).slice(root.length);
@@ -110,8 +103,166 @@ export async function getTableOfContents(
     children.push(child);
   }
 
-  console.log({ children });
-  const toc: TableOfContents = { children: [] };
+  return { children };
+}
 
+export type Node<T> =
+  & T
+  & { children?: Node<T>[] };
+
+function sortChildren<T>(node: Node<T>, fn: (node: Node<T>) => number): void {
+  if (node.children !== undefined) {
+    node.children.sort(fn);
+    node.children.forEach((child) => sortChildren(child, fn));
+  }
+}
+
+// function dfs_sort(entry) {
+//   // If current node has children, sort them
+//   if (entry.children !== undefined) {
+//     // Write a sort function that sorts by string order of the first element in the name field
+//     //entry.children.sort((a, b) => a.name[0].localeCompare(b.name[0]));
+//     entry.children.sort((a, b) =>
+//       a.name.join("/").localeCompare(b.name.join("/"))
+//     );
+//   }
+
+//   // For each child, if they have children, sort them
+//   for (let i = 0; i < entry.children.length; i++) {
+//     if (entry.children[i].children !== undefined) {
+//       dfs_sort(entry.children[i]);
+//     }
+//   }
+// }
+
+const rootKey = "-";
+
+function makeAdjacencies(
+  children: FSItem[],
+): { [key: string]: Node<FSItem>[] } {
+  // Store an adjacency list of the sections and their paths.
+  const adjacencies: { [key: string]: Node<FSItem>[] } = { [rootKey]: [] };
+
+  // Loop through children and store them in the adjacency list.
+  for (
+    const item of children.toSorted((a, b) => a.name.length - b.name.length)
+  ) {
+    const suffix = item.name[item.name.length - 1];
+    if (suffix.startsWith("00") && item.name.length !== 1) {
+      item.name.pop();
+      adjacencies[rootKey].push(item);
+      continue;
+    }
+
+    let path = item.name.slice(0, -1).join("/");
+    path = path === "" ? rootKey : path;
+    if (adjacencies[path] === undefined) {
+      adjacencies[path] = [];
+    }
+
+    adjacencies[path].push(item);
+  }
+
+  return adjacencies;
+}
+
+function makeTableOfContents(
+  children: FSItem[],
+): TableOfContents {
+  const adjacencies = makeAdjacencies(children);
+  console.log({ adjacencies });
+
+  // Loop through the adjacencies and build the table of contents.
+  const paths = Object.keys(adjacencies)
+    .toSorted((b, a) => a.split("/").length - b.split("/").length);
+  for (const path of paths) {
+    // Save the root path for last.
+    if (path === rootKey) {
+      continue;
+    }
+
+    // Get parent path.
+    const parentPath = path.split("/").slice(0, -1).join("/");
+
+    // Find parent.
+    const parent = adjacencies[parentPath]
+      ?.find((item) => item.name.join("/") === path);
+    if (parent === undefined) {
+      continue;
+    }
+
+    // Add children to parent.
+    parent.children = adjacencies[path];
+    delete adjacencies[path];
+  }
+
+  // Add virtual root to children.
+  for (const item of adjacencies[rootKey]) {
+    // WIP: Suffix '00_index' should be omitted from name.
+  }
+
+  console.log({ adjacencies });
+  const toc = { children: [] };
   return toc;
 }
+
+// deno run -A server/docs/docs.ts
+//
+if (import.meta.main) {
+  // TODO: Rename to getFSItems.
+  const toc = await getTableOfContents();
+  // console.log(toc);
+  // TODO: Rename to getTableOfContents.
+  const toc2 = makeTableOfContents(toc.children);
+  // console.log(toc2);
+}
+
+/*
+  // We now have a hashmap of "path/to/a/section" => [section_entry]
+  // Where a section_entry is an object with it's data: {name: ["path", "to", "section"], title: "Section Title", Optional[children]: [], Optional[href]: "https://example.com"}
+
+  // Now loop through our entries by depth
+  const paths = Object.keys(sections);
+  paths.sort((b, a) => a.split("/").length - b.split("/").length);
+  for (let i = 0; i < paths.length; i++) {
+    // Save the root path for last
+    if (paths[i] === "") continue;
+    // Get what our parent's "name" field would be
+    const parent_name_field = paths[i].split("/");
+    // Get where our parent's path would be stored
+    let parent_path = "";
+    if (parent_name_field.length !== 1) {
+      parent_path = parent_name_field.slice(0, -1).join("/");
+    }
+    // Search for our parent
+    for (let j = 0; j < sections[parent_path].length; j++) {
+      // If their field matches what would be our field
+      if (
+        sections[parent_path][j].name.join("/") == parent_name_field.join("/")
+      ) {
+        // Add our children to our parent
+        sections[parent_path][j].children = sections[paths[i]];
+        // Remove our children from the list of sections
+        delete sections[paths[i]];
+        break;
+      } else {
+      }
+    }
+  }
+
+  // Finally, re_add the "00_index" to children at root
+  for (let i = 0; i < sections[""].length; i++) {
+    const name_field = sections[""][i].name;
+    if (name_field[0] === "00_index" || name_field[0].slice(0, 2) == "99") {
+      continue;
+    } else {
+      name_field.push("00_index");
+    }
+  }
+
+  // Sort titles
+  const ret = { children: sections[""] };
+  dfs_sort(ret);
+  return ret;
+}
+*/
